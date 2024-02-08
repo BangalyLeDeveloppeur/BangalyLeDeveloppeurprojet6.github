eb636c38b762f0f7791af36b563e21c3a39df74d
BangalyLeDeveloppeur <condebangaly580@gmail.com>
2024-02-09 00:23:31 +0100

    envoie du projet6 sur github

---
+++ FrontEnd/assets/index.js
@@ -0,0 +1,128 @@
+//// recuperation dans api ///
+const gallery = document.querySelector("main");
+const filter = document.querySelector(".filter");
+const gallerySection = document.querySelector(".gallery");
+
+async function getworks() {
+  const Response = await fetch(`http://localhost:5678/api/works/`);
+  return await Response.json();
+}
+getworks();
+
+//afficharge des travaux ///
+async function affichageTravaux(element) {
+  const arrayWorks = await getworks();
+  arrayWorks.forEach((works) => {
+    //la boucle forEch a chaque passage dans la base de donées
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    const figcaption = document.createElement("figcaption");
+    img.src = works.imageUrl;
+    figcaption.textContent = works.title;
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    gallerySection.appendChild(figure);
+  });
+}
+affichageTravaux();
+
+/// recuperation du tableau categorie dans la base //
+async function getcategorie() {
+  const Reponse = await fetch("http://localhost:5678/api/categories/");
+  return await Reponse.json();
+}
+getcategorie();
+
+async function categoriesButtons() {
+  const categorys = await getcategorie();
+  // console.log(categorys);
+  categorys.forEach((category) => {
+    const btn = document.createElement("button");
+    btn.textContent = category.name;
+    btn.id = category.id;
+    filter.appendChild(btn);
+  });
+}
+categoriesButtons();
+
+///affichage de filtre des contenus par categorie//
+async function filtreCategory() {
+  const appart = await getworks();
+  console.log(appart);
+  const buttons = document.querySelectorAll(".filter button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      gallerySection.innerHTML = "";
+      btnId = e.target.id;
+      if (btnId != "0") {
+        const filtreParCategory = appart.filter((category) => {
+          return category.categoryId == btnId;
+        });
+        filtreParCategory.forEach(category => {
+          affichageTravaux(category);
+        });
+      }
+      console.log(btnId);
+    });
+  });
+}
+filtreCategory();
+
+// la page de connexion//
+
+
+////4444
+
+//const gallerySection = document.querySelector(".gallery");
+//const categorySection = document.querySelector(".category");
+
+//const getWork = async (category) => {
+//const response = await fetch("http://localhost:5678/api/works");
+//const data = await response.json();
+//console.log(category);
+//if (!category || category === 0) {
+// return data;
+//} else {
+//return data.filter((element) => element.categoryId === category);
+//}
+//};
+
+//const getCategory = async () => {
+//const response = await fetch("http://localhost:5678/api/categories");
+///const data = await response.json();
+//return data;
+//};
+
+//const afficherlesTravaux = async (works) => {
+// gallerySection.innerHTML = "";
+//works.forEach((element) => {
+// const workDOM = `
+//<figure>
+//  <img src="${element.imageUrl}" alt="${element.title}" width="150"/>
+//   <figcaption>${element.title} ${element.categoryId}<figcaption>
+// <figure>
+// `;
+// // gallerySection.innerHTML += workDOM;
+//});
+//};
+
+//const filterWork = async (category) => {
+// const works = await getWork(category);
+// afficherlesTravaux(works);
+//};
+
+//categorySection.addEventListener("click", (event) => {
+//const element = event.target;
+//const button = element.tagName.toLowerCase();
+//(button === "button") {
+//const idCategory = element.getAttribute("id");
+//filterWork(Number.parseInt(idCategory));
+//}
+
+//});
+
+//const init = async () => {
+//const works = await getWork();
+//afficherlesTravaux(works);
+//};
+//init();
---
+++ FrontEnd/login.js
@@ -0,0 +1,50 @@
+// mes varaiables
+const email = document.querySelector(" form #email");
+const passord = document.querySelector("form #password");
+const form = document.querySelector("form");
+const messageErreur = document.querySelector(".logg p");
+
+// la partie post//
+
+const PostUser = {
+  method: "Post",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    pseudo: "email",
+    password: "password",
+    mode: "cors",
+    Credential: "sans-origin",
+  }),
+};
+
+async function userPost() {
+  fetch("http://localhost:5678/api/users/", PostUser ).thin(() =>
+  console.log("data envoyée")
+  );
+}
+//function de conexion
+async function login() {
+  const user = await userPost();
+  form.addEventListener("submit", (e) => {
+    e.preventDeFault();
+    const UserEmail = email.value;
+    const UserPword = password.value;
+    /// console.log(UserEmail, UserPword);
+    user.forEach((e) => {
+      if (email.value == UserEmail && password.value == UserPword) {
+        // si toutes ces conditions son rempliees
+        window.localStorage.loged = true;
+        window.localStorage.href = "./index.html";
+      } else {
+        // message erreur
+        email.classList.add("inputErrorLogin");
+        password.classList.add("inputErrorLogin");
+        messageErreur.textContent =
+          "Votre email ou votre de pass est incorrect";
+      }
+    });
+  });
+}
+login();
